8622cc21cc961a7b25fa3df55e4eebbdcd55ff31
Fate2664 <18pielou@gmail.com>
2025-03-26 00:04:50 +0200

    Updated the road spawner, still a bit confused about some stuff but I think I'm getting it.

--- GameDev-EndlessRunner/Assets/Scripts/RoadSpawner.cs
+++ GameDev-EndlessRunner/Assets/Scripts/RoadSpawner.cs
@@ -1,27 +1,32 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class RoadSpawner : MonoBehaviour
 {
     public List<GameObject> roads;
-    private float offset = 155f;
+    private float Zoffset = 155f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      if (roads != null && roads.Count > 0)
+        if (roads != null && roads.Count > 0)
         {
             roads = roads.OrderByDescending(r => r.transform.position.z).ToList();
-        }  
+        }
     }
 
     public void MoveRoad()
     {
         GameObject movedRoad = roads[0];
+        float newZoffest = roads[roads.Count - 1].transform.position.z - Zoffset;
+        float newYoffset = 8 + 3.256991f;
+        float newXoffset = -3.64609f;
         roads.RemoveAt(0);
-        float newZoffest = roads[roads.Count - 1].transform.position.z - offset;
-        movedRoad.transform.position = new Vector3(0, 0 , newZoffest);
+        //idk why I need to add an x and y offset but if I don't, the roads spawn somewhere else??????????
+        movedRoad.transform.position = new Vector3(newXoffset, newYoffset, newZoffest);
         roads.Add(movedRoad);
     }
 
