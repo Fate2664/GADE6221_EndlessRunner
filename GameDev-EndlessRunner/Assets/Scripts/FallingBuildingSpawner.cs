2bf4750210fb174ed4f85de109dac0af79f56843
Nicholas Smart <160883116+NickSmart14@users.noreply.github.com>
2025-03-27 19:18:53 +0200

    Added spawner for buildings and car.
    Distance that is spawns may need to be adjusted. Also the truck does not spawn correctly. I was intending for it to cross the road. Also I will either rename the spawner script or make a separate spawner for the truck

---
+++ GameDev-EndlessRunner/Assets/Scripts/FallingBuildingSpawner.cs
@@ -0,0 +1,55 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingBuildingSpawner : MonoBehaviour
+{
+  
+    float SpawnRate = 5f;
+    float Counter = 0f;
+    
+
+    public List<GameObject> Buildings;
+    public Transform Player;
+
+    public float DistanceAhead = 30f;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpawnFallingBuilding();
+    }
+
+    private void SpawnFallingBuilding()
+    {
+        Counter += Time.deltaTime;
+       
+
+        if(Counter >= SpawnRate)
+        {
+
+            GameObject SpawnBuilding = Buildings[0];
+
+            if (Buildings.Count > 0 && Player != null)
+            {
+                int randomIndex = Random.Range(0, Buildings.Count);
+                SpawnBuilding = Buildings[randomIndex];
+
+                Vector3 SpawnPosition = new Vector3(-78, 0, Player.position.z - DistanceAhead);
+
+                 Instantiate(SpawnBuilding,SpawnPosition, Quaternion.identity);
+
+
+            }
+
+            
+         
+                Counter = 0f;
+
+        }
+
+       
+
+    }
+}
